e4f1d2ed2e400abece632d667b8d0ba1f57a858b
arnold.coto@gmail.com <arnoldcoto@gmail.com>
2023-01-21 01:02:01 -0600

    cuartlop commit

--- appdjango/settings.py
+++ appdjango/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+#SECRET_KEY = os.getenv("SECRET_KEY")
 
 
-#SECRET_KEY = 'django-insecure-*r5i)jf6p=bo6_ftysw10-s353*%o)uq)*$tyt&g0oar)joom4'
+SECRET_KEY = 'django-insecure-*r5i)jf6p=bo6_ftysw10-s353*%o)uq)*$tyt&g0oar)joom4'
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
