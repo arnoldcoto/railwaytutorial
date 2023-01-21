ac79aad98de86b85d66050d4c7fe612e7fbac36c
arnold.coto@gmail.com <arnoldcoto@gmail.com>
2023-01-21 01:00:00 -0600

    cuartoop commit

--- appdjango/settings.py
+++ appdjango/settings.py
@@ -26,8 +26,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("SECRET_KEY")
 
+
+#SECRET_KEY = 'django-insecure-*r5i)jf6p=bo6_ftysw10-s353*%o)uq)*$tyt&g0oar)joom4'
+
+
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
